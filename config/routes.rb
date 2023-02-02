59f761fb5772c6ea02c69c12db0e82716f9657c3
takuya sakamoto <sthtcwm@yahoo.co.jp>
2023-02-03 02:58:00 +0900

    kadaiok

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
+  post "/graphql", to: "graphql#execute"
+  root to: 'blogs#index'
+  resources :blogs
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # if Rails.env.development?
+  #   mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/your/endpoint"
+  # end
+end
